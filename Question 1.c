54a0b4b30a516bf6ab61062759746362e7c39462
BulwanyiIsaac <38222076+BulwanyiIsaac@users.noreply.github.com>
2018-04-12 10:05:54 +0300

    Add files via upload

---
+++ Question 1.c
@@ -0,0 +1,22 @@
+// BULWANYI ISAAC
+//17/U/211
+//217000098
+//BSTE
+
+//QUESTION 0NE
+
+
+#include <stdio.h>
+
+int x = 1;
+
+main ()
+{
+    if(x == 1)
+        printf("x equals 1");
+
+    else
+        printf("x does not equal 1");
+
+        return 0;
+}
